576bf645ba88efd1fda018f67eed81c2759aa46a
Nathaniel Angafor <69981421+nathanielangafor@users.noreply.github.com>
2022-08-19 05:26:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // File imports
 const utilities = require('./utilities'); // Utility functions
-const database = require('./database'); // Utility functions
+// const database = require('./database'); // Database functions
 
 // Module for creating web client
 const express = require('express');
@@ -8,27 +8,43 @@ const app = express();
 const expressip = require('express-ip');
 const port = 3000
 
-// Useful modules
-const prompt = require("prompt-sync")({ sigint: true }); // Allows for user input
-
 app.use(expressip().getIpInfoMiddleware);
 
-// Settings
-var recipientEmail = prompt("What is the recipient Email?");
-var emailSubject = prompt("What is the emailSubject?");
-var today = new Date().toISOString().slice(0, 10);
-var emailHash = utilities.generateSha256(recipientEmail + ':' + emailSubject + ':' + today); // Make time more accurate by including hour/minute
+app.get('/generateHash', (req, res) => {
+  // Confirms the perameters recieverEmailAddress is present
+  if (req.query.recieverEmailAddress != undefined && req.query.recieverEmailAddress != '') {
+    // Confirms the perameters emailSubject is present
+    if (req.query.emailSubject != undefined && req.query.emailSubject != '') {
+      
+      // Get hash generation data from request form
+      var recieverEmailAddress = req.query.recieverEmailAddress;
+      var emailSubject = req.query.emailSubject;
+      var today = new Date().toISOString().slice(0, 10);
+      var hash = utilities.generateSha256(recieverEmailAddress + ':' + emailSubject + ':' + today); // Make time more accurate by including hour/minute
 
-console.log('Your generated link embed - http://127.0.0.1:3000/emailBeacon?hash=' + emailHash) // Make this ACTUALLY html 
-console.log('\nHow to use:\nPaste this html code at the bottom of your email and monitor your dashboard to know when the email has been read!')
+      // Append data to database
+      // database.insertLink(recieverEmailAddress, emailSubject, hash, today);
+
+      // Return embed link
+      res.send(`{'success': 'http://127.0.0.1:3000/emailBeacon?hash=${hash}'}`);
+    } else {
+      res.send("{'error': 'Please add the emailSubject perameter.'}");
+    }
+  } else {
+    res.send("{'error': 'Please add the recieverEmailAddress perameter.'}");
+  }
+});
 
 app.get('/emailBeacon', (req, res) => {
+  // Confirms the perameters hash is present
   if (req.query.hash != undefined && req.query.hash != '') {
     // Update database to mark email as read using hash input
-    console.log('Email with hash -- ' + req.query.hash + ' -- has been read.')
+    database.update('trackingLinks', 'status', 'read', 'hash', req.query.hash)
+
+    // Render image
     res.sendFile('/Users/fnln/Desktop/Email Beacon/ket.jpeg');
   } else {
-    res.send('invalidHash');
+    res.send("{'error': 'Please add the hash perameter.'}");
   }
 });
 
@@ -40,4 +56,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
 
