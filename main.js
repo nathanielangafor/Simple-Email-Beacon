ba3c9a2224fe5da19935eb93ec70953944194755
Nathaniel Angafor <69981421+nathanielangafor@users.noreply.github.com>
2022-08-21 18:52:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,26 @@
 // File imports
-const utilities = require('./utilities'); // Utility functions
 // const database = require('./database'); // Database functions
 
-// Module for creating web client
+// Settings for creating web client
 const express = require('express');
 const app = express();
 const expressip = require('express-ip');
 const port = 3000
 
+// Other useful settings
+const crypto = require('crypto');
+
 app.use(expressip().getIpInfoMiddleware);
 
 app.get('/generateHash', (req, res) => {
-  // Confirms the perameters recieverEmailAddress is present
-  if (req.query.recieverEmailAddress != undefined && req.query.recieverEmailAddress != '') {
-    // Confirms the perameters emailSubject is present
-    if (req.query.emailSubject != undefined && req.query.emailSubject != '') {
-      
-      // Get hash generation data from request form
-      var recieverEmailAddress = req.query.recieverEmailAddress;
-      var emailSubject = req.query.emailSubject;
-      var today = new Date().toISOString().slice(0, 10);
-      var hash = utilities.generateSha256(recieverEmailAddress + ':' + emailSubject + ':' + today); // Make time more accurate by including hour/minute
+  // Generate hash with client IP address and UNIX time
+  var hash = crypto.randomUUID(); // Make time more accurate by including hour/minute
 
-      // Append data to database
-      // database.insertLink(recieverEmailAddress, emailSubject, hash, today);
+  // Append data to database
+  // database.insertLink(recieverEmailAddress, emailSubject, hash, today);
 
-      // Return embed link
-      res.send(`{'success': 'http://127.0.0.1:3000/emailBeacon?hash=${hash}'}`);
-    } else {
-      res.send("{'error': 'Please add the emailSubject perameter.'}");
-    }
-  } else {
-    res.send("{'error': 'Please add the recieverEmailAddress perameter.'}");
-  }
+  // Return embed link
+  res.send(`{'success': 'http://127.0.0.1:3000/emailBeacon?hash=${hash}'}`);
 });
 
 app.get('/emailBeacon', (req, res) => {
@@ -56,5 +44,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
 
 
