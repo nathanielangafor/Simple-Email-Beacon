807dbce7741e9ac90f3094a2d7fb4189e3749184
Nathaniel Angafor <69981421+nathanielangafor@users.noreply.github.com>
2022-08-22 05:55:28 -0500

    Update database.js

--- database.js
+++ database.js
@@ -8,19 +8,16 @@ const db = new sqlite3.Database("./database.db", sqlite3.OPEN_READWRITE, (err) =
 
 function createLinksTable() {
   // Create the initial table to store tracking links and their status
-  db.run('CREATE TABLE trackingLinks (id INTEGER PRIMARY KEY, recieverEmailAddress TEXT, emailSubject TEXT, timeSent TEXT, emailHash text, status TEXT)');
+  db.run('CREATE TABLE trackingLinks (id INTEGER PRIMARY KEY, name TEXT, timeSent TEXT, timeFirstRead TEXT, loggedIPAddresses TEXT, cordinates TEXT, emailUUID text, status TEXT)');
 }
 
-function insertLink(recieverEmailAddress, emailSubject) {
-  // Gets today's date
-  var today = new Date().toISOString().slice(0, 10);
-  // Generates a hash with all the relevant details
-  // Make time more accurate by including hour/minute for the purpose of scalability
-  var emailHash = utilities.generateSha256(recieverEmailAddress + ':' + emailSubject + ':' + today);
-  // Status will always be unread until edited by web client
+function insertLink(name, emailUUID, timeSent, timeFirstRead) {
+  // status, loggedIPAddresses and cordinates will be set on the first get request
   var status = 'unread';
+  var loggedIPAddresses = '';
+  var cordinates = '';
 
-  db.run('INSERT INTO trackingLinks (recieverEmailAddress, emailSubject, timeSent, emailHash, status) VALUES (?,?,?,?,?)', [recieverEmailAddress, emailSubject, today, emailHash, status], (err) => {
+  db.run('INSERT INTO trackingLinks (name, timeSent, timeFirstRead, loggedIPAddresses, cordinates, emailUUID, status) VALUES (?,?,?,?,?,?,?)', [name, timeSent, timeFirstRead, loggedIPAddresses, cordinates, emailUUID, status], (err) => {
       if (err) return console.error(err.message);
   });
 }
@@ -29,7 +26,7 @@ function read(tableName){
   // Returns all the data in a given table
   db.all(`SELECT * FROM ${tableName}`, [], (err, rows) => {
       if(err) return console.error(err.message);
-          return rows;
+          console.log(rows);
   });
 } 
 
@@ -37,12 +34,11 @@ function deleteData(tableName, columnName, columnValue){
   // Deletes given data from a table using the value from one of its columns
   db.run(`DELETE FROM ${tableName} WHERE ${columnName}=?`, columnValue, err =>{
       if(err) return console.log(err.message);
-      console.log(`Item ${columnValue} in column ${columnName} deleted successfully...`);
   });
 }
 
 function update(tableName, columnName1, columnValue1, columnName2, columnValue2) {
   // Updates a given location in the data table based on inputed data
-  db.run(`UPDATE ${tableName} SET ${columnName1}=? WHERE ${columnName2}=?`, [columnValue1, columnValue2], function(err,rows){
+    db.run(`UPDATE ${tableName} SET ${columnName1}=? WHERE ${columnName2}=?`, [columnValue1, columnValue2], function(err,rows){
   });
 }
