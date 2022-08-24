223eb9d7413e1c9f1cdd9406e8557aa50fc6964e
Nathaniel Angafor <69981421+nathanielangafor@users.noreply.github.com>
2022-08-23 18:43:12 -1000

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const { lookup } = require('geoip-lite');
 
 // Settings to initilize Express web client
 const app = express();
-const port = 3000
+const port = 8080
 app.use(expressip().getIpInfoMiddleware);
 
 
@@ -20,9 +20,9 @@ app.get('/generateUUID', (req, res) => {
   // Generate UUID
   var UUID = crypto.randomUUID(); // Make time more accurate by including hour/minute
   // Append data to database
-  database.insertLink('', UUID, Date.now(), 'null');
+  database.insertLink('', UUID, Date.now());
   // Return embed link
-  res.send(`{'success': 'http://127.0.0.1:3000/emailBeacon?UUID=${UUID}'}`);
+  res.send(`http://127.0.0.1:3000/emailBeacon?UUID=${UUID}`);
 });
 
 
@@ -30,26 +30,17 @@ app.get('/generateUUID', (req, res) => {
 app.get('/emailBeacon', (req, res) => {
   // Confirms the perameters UUID is present
   if (req.query.UUID != undefined && req.query.UUID != '') {
-    // Get trackingLinks data table
-    var data = database.read('trackingLinks');
+  // Get the client IP address
+    // var ip = req.headers['x-forwarded-for']
+    var ip = '73.134.52.248'
+    // Insert a visit entry into the db
+    database.insertVisit(Date.now(), ip, JSON.stringify(lookup(ip)), req.query.UUID)
 
-    // Update database to mark email as read using UUID input
-    database.update('trackingLinks', 'status', 'read', 'UUID', req.query.UUID)
-    // Isolate the data entry with the UUID input from the client server
-    for (var i = 0; i < data.length; i++) {
-      if (data[i][1] == req.query.UUID) {
-        var loggedIPAddresses = data[i][3];
-        var cordinates = data[i][4];
-      }
-    }
-    // Update IP addresses that have visited an email
-    database.update('trackingLinks', 'loggedIPAddresses', loggedIPAddresses + ':' + req.socket.remoteAddress, 'UUID', req.query.UUID)
-    database.update('trackingLinks', 'cordinates', cordinates + ':' + String(lookup(loggedIPAddresses)['ll']), 'UUID', req.query.UUID)
-    
     // Render image
-    res.sendFile('/Users/fnln/Desktop/Email Beacon/1x1_image.png');
+    res.sendFile('/Users/appleid/Downloads/Simple-Email-Beacon-main/1x1_image.png');
+
   } else {
-    res.send("{'error': 'Please add the UUID perameter.'}");
+    res.send("Error, please add the UUID perameter.");
   }
 });
 
@@ -58,17 +49,16 @@ app.get('/emailBeacon', (req, res) => {
 app.get('/emailBeaconStatus', (req, res) => {
   // Confirms the perameters UUID is present
   if (req.query.UUID != undefined && req.query.UUID != '') {
-    // Get trackingLinks data table
-    var data = database.read('trackingLinks');
-    var result = 'unread'
-    // Isolate the data entry with the UUID input from the client server
-    for (var i = 0; i < data.length; i++) {
-      if (data[i][1] == req.query.UUID) {
-        result = 'read'; 
+    // fetch all links visits for a given UUID
+    database.readSome('linkVisits', 'emailUUID', req.query.UUID, (rows) => {
+      var visits = rows;
+      // If there are no visits, return Unread... otherwise return list of visits
+      if (visits.length == 0) {
+        res.send('Unread...')
+      } else {
+        res.send(visits)
       }
-    }
-    // Return the result of the beacon
-    res.send(result)
+    });
   } else {
     res.send("{'error': 'Please add the UUID perameter.'}");
   }
@@ -84,6 +74,8 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
 
 
 
